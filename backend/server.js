593750ccfc0800a5b63b819b518285bbf799c39c
janhvisoni902 <janhvisoni902@gmail.com>
2025-12-01 16:05:35 +0530

    Fix CORS for Netlify deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,20 @@ const connectDb = require("./src/db/db");
 
 connectDb();
 
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://melodious-pastelito-533ecd.netlify.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
 
